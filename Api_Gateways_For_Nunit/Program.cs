2da71dbec1feaf04ddb227976339313b2c9a6d6f
Moinuddin Ali <moinuddinali@rathi.com>
2024-05-06 15:08:40 +0530

    New Added

---
+++ Api_Gateways_For_Nunit/Program.cs
@@ -0,0 +1,42 @@
+using Consul;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configure the consul
+var consulClient = new ConsulClient();
+builder.Configuration.AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration).AddConsul().AddPolly();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.UseOcelot().Wait();
+
+app.MapControllers();
+
+app.Run();
